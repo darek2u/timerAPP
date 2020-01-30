da21c38444e7bc11b08cda2a21a8a2a8bbf3cb18
Dariusz Urbanczyk <dare2u@gmail.com>
2020-01-30 09:34:23 +0000

    Initial Commit

---
+++ timerAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  timerAPP
+//
+//  Created by Dariusz Urbanczyk on 30/01/2020.
+//  Copyright © 2020 Dariusz Urbanczyk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
